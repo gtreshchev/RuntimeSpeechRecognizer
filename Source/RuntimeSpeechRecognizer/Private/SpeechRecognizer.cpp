ce4f4b2f0c7d4cbe1a10bb5fe847b9b5b73f6d87
Georgy Treshchev <gtreshchev@gmail.com>
2024-01-14 10:01:16 +0000

    Year update from 2023 to 2024

--- Source/RuntimeSpeechRecognizer/Private/SpeechRecognizer.cpp
+++ Source/RuntimeSpeechRecognizer/Private/SpeechRecognizer.cpp
@@ -1,4 +1,4 @@
-﻿// Georgy Treshchev 2023.
+﻿// Georgy Treshchev 2024.
 
 #include "SpeechRecognizer.h"
 #include "SpeechRecognizerDefines.h"
--- Source/RuntimeSpeechRecognizer/Private/SpeechRecognizerModel.cpp
+++ Source/RuntimeSpeechRecognizer/Private/SpeechRecognizerModel.cpp
@@ -1,4 +1,4 @@
-﻿// Georgy Treshchev 2023.
+﻿// Georgy Treshchev 2024.
 
 #include "SpeechRecognizerModel.h"
 #include "SpeechRecognizerDefines.h"
