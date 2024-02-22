00d52527412a5fcffd6b9da7cf2368e0bed98fb1
Georgy Treshchev <gtreshchev@gmail.com>
2024-02-22 20:27:29 +0000

    Do not set BULKDATA_Force_NOT_InlinePayload and BULKDATA_Size64Bit to avoid crashes on certain platforms (especially on Quest 3)
    - Do not set BULKDATA_Force_NOT_InlinePayload as it sometimes leads to crashes on Quest 3 and possibly other platforms
    - Do not set BULKDATA_Size64Bit as it's automatically set by the engine when the data is larger than 2GB

--- Source/RuntimeSpeechRecognizer/Private/SpeechRecognizerModel.cpp
+++ Source/RuntimeSpeechRecognizer/Private/SpeechRecognizerModel.cpp
@@ -7,8 +7,9 @@ void USpeechRecognizerModel::Serialize(FArchive& Ar)
 {
 	Super::Serialize(Ar);
 
-	// Set the bulk data flags to force loading the data on-demand and to use 64-bit size
-	LanguageModelBulkData.SetBulkDataFlags(BULKDATA_Force_NOT_InlinePayload | BULKDATA_Size64Bit);
+	// BULKDATA_Force_NOT_InlinePayload sometimes leads to crashes on Quest 3 and possibly other platforms, so it's removed
+	// BULKDATA_Size64Bit is automatically set by the engine when the data is larger than 2GB, so it's not necessary to set it manually
+	// LanguageModelBulkData.SetBulkDataFlags(BULKDATA_Force_NOT_InlinePayload | BULKDATA_Size64Bit);
 
 	LanguageModelBulkData.Serialize(Ar, this, INDEX_NONE, false);
 	UE_LOG(LogRuntimeSpeechRecognizer, Log, TEXT("Serializing language model data with the size of %lld bytes"), LanguageModelBulkData.GetBulkDataSize());
